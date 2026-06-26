2fb2af5a5c67d4fa2598b2edd251ff36bb05d279
luis-octavius <luisoctavius.sc@gmail.com>
2026-06-26 14:59:08 -0300

    feat: add section 'Sobre'
    - styles for Sobre
    - add assets to keep the needed photos

---
+++ index.js
@@ -0,0 +1,40 @@
+const profileImages = [
+    'me.png',
+    'me-2.jpg'
+];
+
+let indexImg = 0;
+
+function main() {
+    const sobre = document.querySelector("#sobre");
+
+    changeImage();
+
+    initSlideShow();
+}
+
+function initSlideShow() {
+    setInterval(changeImage, 3000);
+}
+
+function changeImage() {
+    const profilePhoto = document.querySelector("#profile-photo");
+
+    indexImg = (indexImg + 1) % profileImages.length; 
+
+    const path = `assets/${profileImages[indexImg]}`;
+
+    profilePhoto.style.transition = 'opacity 0.4s ease';
+    profilePhoto.style.opacity = '0';
+
+    setTimeout(() => {
+        profilePhoto.src = path; 
+
+        void profilePhoto.offsetHeight; 
+
+        profilePhoto.style.opacity = '1';
+    }, 400)
+}    
+
+
+main();
